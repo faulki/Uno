89ced87c660663e1f6a102588b106a2e09f94a39
Killian <nydefx@gmail.com>
2025-03-03 14:50:52 +0100

    First commit
    first commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import {io} from "socket.io-client";
+
+const socket = io("http://localhost:3000/");
+var pseudo: string | any[];
+let listPlayer;
+
+// let players[] = socket.on('updatePLayers', player)
+
+socket.on("joinGameStatus", (message) =>{
+  console.log(message);
+});
+
+listPlayer = document.getElementById('listplayer');
+
+document.getElementById('pseudo')!.addEventListener('input', function(event) {
+  const element = event.target! as HTMLInputElement;
+  pseudo = element.value;
+});
+
+
+
+document.getElementById('joinGame')!.addEventListener("click", function(){
+  if (pseudo.length < 1){
+    alert("Veuillez entrer un pseudo");
+  }
+  else {
+    socket.emit('joinGame', (pseudo))
+  }
+})
+
+listPlayer?.textContent = socket.emit
