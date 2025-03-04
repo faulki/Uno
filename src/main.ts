963c02c972ef2c73d3214fd5d649564ee95ac307
faulki <nydefx@gmail.com>
2025-03-04 12:07:39 +0100

    tetetet

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ const socket = io("http://localhost:3000/");
 var pseudo: string | any[];
 let listPlayer = document.getElementById("listPlayer");
 let verify = false;
+let buttonReady = false;
 
 // let players[] = socket.on('updatePLayers', player)
 
@@ -31,18 +32,37 @@ document.getElementById('joinGame')!.addEventListener("click", function(){
 
 //Fonction pour mettre le joueur prêt
 document.getElementById('ready')!.addEventListener("click", function(){
-  if (verify) {
+  if (verify && buttonReady == false) {
     socket.emit('playerReady');
+    buttonReady = true;
+    document.getElementById('ready')!.innerHTML = "Not Ready Anymore"
+  }
+  else if (verify && buttonReady == true) {
+    console.log('notReady')
+    socket.emit('playerNotReady');
+    buttonReady = false;
+    document.getElementById('ready')!.innerHTML = "Ready"
   }
 })
 
 socket.on('updatePlayers', (players) => {
   listPlayer!.innerHTML = "";
   for (let i = 0; i < players.length; i++) {
-    console.log(players[i].name);
+    console.log(players[i]);
     const element = document.createElement('p');
-    element.innerHTML = "<p>" + players[i].name + " joined the game</p>";
+    element.innerHTML = "<p id="+players[i].name+">" + players[i].name + " joined the game</p>";
     listPlayer!.appendChild(element);
+
+    if (players[i].isReady == true) {
+      let playerReady = document.getElementById(players[i].name);
+      playerReady!.style.color = "green";
+      playerReady!.textContent = players[i].name + " is ready";
+    }
+    else if (players[i].isReady == false) {
+      let playerNotReady = document.getElementById(players[i].name);
+      playerNotReady!.style.color = "white";
+      playerNotReady!.textContent = players[i].name + " joined the game";
+    }
     // document.getElementById('listPlayer')!.innerHTML += "<p>" + players[i].name + " joined the game</p>";
   }
 })
